27ee24be6d69045c0f0051572231836de232d39c
Rodrigo de Almeida Francisco <rodrigoalmeidafrancisco@gmail.com>
2023-05-23 00:20:28 -0300

    Criado mais Testes Unitários

--- Project/Developer.ExtensionCore/DecimalExtension.cs
+++ Project/Developer.ExtensionCore/DecimalExtension.cs
@@ -35,34 +35,6 @@ namespace Developer.ExtensionCore
 
         /// <summary>
         /// Converte uma string em Decimal.
-        /// Caso não consiga converter, retorna null.
-        /// </summary>
-        /// <param name="val"></param>
-        /// <returns></returns>
-        public static decimal? ToDecimalNull(this string val)
-        {
-            decimal? valueReturn = null;
-
-            try
-            {
-                if (val.IsNullOrEmptyOrWhiteSpace() == false)
-                {
-                    if (decimal.TryParse(val, out decimal aux))
-                    {
-                        valueReturn = aux;
-                    }
-                }
-            }
-            catch
-            {
-                valueReturn = null;
-            }
-
-            return valueReturn;
-        }
-
-        /// <summary>
-        /// Converte uma string em Decimal.
         /// Caso não consiga converter, retorna decimal.MinValue.
         /// </summary>
         /// <param name="val"></param>
@@ -102,6 +74,78 @@ namespace Developer.ExtensionCore
         public static decimal ToDecimal(this string val, NumberStyles style, EnumCultureInfo cultureInfo)
         {
             decimal valueReturn = val.ToDecimal(style, cultureInfo.GetDescription());
+            return valueReturn;
+        }
+
+        /// <summary>
+        /// Converte uma string em Decimal.
+        /// Caso não consiga converter, retorna null.
+        /// </summary>
+        /// <param name="val"></param>
+        /// <returns></returns>
+        public static decimal? ToDecimalNull(this string val)
+        {
+            decimal? valueReturn = null;
+
+            try
+            {
+                if (val.IsNullOrEmptyOrWhiteSpace() == false)
+                {
+                    if (decimal.TryParse(val, out decimal aux))
+                    {
+                        valueReturn = aux;
+                    }
+                }
+            }
+            catch
+            {
+                valueReturn = null;
+            }
+
+            return valueReturn;
+        }
+
+        /// <summary>
+        /// Converte uma string em Decimal.
+        /// Caso não consiga converter, retorna null.
+        /// </summary>
+        /// <param name="val"></param>
+        /// <param name="style"></param>
+        /// <param name="cultureInfo"></param>
+        /// <returns></returns>
+        public static decimal? ToDecimalNull(this string val, NumberStyles style, string cultureInfo)
+        {
+            decimal? valueReturn = null;
+
+            try
+            {
+                if (val.IsNullOrEmptyOrWhiteSpace() == false)
+                {
+                    if (decimal.TryParse(val, style, new CultureInfo(cultureInfo), out decimal aux))
+                    {
+                        valueReturn = aux;
+                    }
+                }
+            }
+            catch
+            {
+                valueReturn = null;
+            }
+
+            return valueReturn;
+        }
+
+        /// <summary>
+        /// Converte uma string em Decimal.
+        /// Caso não consiga converter, retorna null.
+        /// </summary>
+        /// <param name="val"></param>
+        /// <param name="style"></param>
+        /// <param name="cultureInfo"></param>
+        /// <returns></returns>
+        public static decimal? ToDecimalNull(this string val, NumberStyles style, EnumCultureInfo cultureInfo)
+        {
+            decimal? valueReturn = val.ToDecimalNull(style, cultureInfo.GetDescription());
             return valueReturn;
         }
 
