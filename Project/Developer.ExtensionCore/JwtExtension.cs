d2e42d2e78f7916c72c2b94069405b0775b8d347
Rodrigo de Almeida Francisco <rodrigoalmeidafrancisco@gmail.com>
2023-05-29 23:56:42 -0300

    Criado novos métodos e teste unitários para o JwtExtension.

--- Project/Developer.ExtensionCore/JwtExtension.cs
+++ Project/Developer.ExtensionCore/JwtExtension.cs
@@ -1,44 +1,132 @@
-﻿using System.Collections.Generic;
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Text;
 
 namespace Developer.ExtensionCore
 {
     public static class JwtExtension
     {
-        public static string GetValue(string token, string claimType)
+        /// <summary>
+        /// Cria um token de acesso, baseado nas informações fornecidas.
+        /// Caso ocorra erro na geração do token, retorna null.
+        /// </summary>
+        /// <param name="additionalHeaderClaims"></param>
+        /// <param name="additionalInnerHeaderClaims"></param>
+        /// <param name="audience"></param>
+        /// <param name="claims"></param>
+        /// <param name="compressionAlgorithm"></param>
+        /// <param name="encryptingCredentials"></param>
+        /// <param name="minutesExpires"></param>
+        /// <param name="issuer"></param>
+        /// <param name="signingCredentials"></param>
+        /// <param name="claimsIdentity"></param>
+        /// <param name="tokenType"></param>
+        /// <returns></returns>
+        public static string CreateTokenJWT(Dictionary<string, object> additionalHeaderClaims, Dictionary<string, object> additionalInnerHeaderClaims,
+            string audience, Dictionary<string, object> claims, string compressionAlgorithm, EncryptingCredentials encryptingCredentials, int minutesExpires,
+            string issuer, SigningCredentials signingCredentials, ClaimsIdentity claimsIdentity, string tokenType)
         {
-            string value = string.Empty;
-
-            if (token.IsNullOrEmptyOrWhiteSpace() == false)
+            try
             {
-                JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
-                JwtSecurityToken jwtToken = handler.ReadJwtToken(token);
-                Claim claim = jwtToken?.Claims.Where(x => x.Type == claimType || x.Type.Contains(claimType)).FirstOrDefault();
-                value = claim?.Value;
-            }
+                SecurityTokenDescriptor tokenDescriptor = new SecurityTokenDescriptor()
+                {
+                    AdditionalHeaderClaims = additionalHeaderClaims,
+                    AdditionalInnerHeaderClaims = additionalInnerHeaderClaims,
+                    Audience = audience,
+                    Claims = claims,
+                    CompressionAlgorithm = compressionAlgorithm,
+                    EncryptingCredentials = encryptingCredentials,
+                    Expires = DateTime.UtcNow.AddMinutes(minutesExpires),
+                    IssuedAt = DateTime.UtcNow,
+                    Issuer = issuer,
+                    NotBefore = DateTime.UtcNow,
+                    SigningCredentials = signingCredentials,
+                    Subject = claimsIdentity,
+                    TokenType = tokenType
+                };
 
-            return value;
+                JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+                SecurityToken securityToken = tokenHandler.CreateToken(tokenDescriptor);
+                string token = tokenHandler.WriteToken(securityToken);
+                return token;
+            }
+            catch
+            {
+                return null;
+            }
         }
 
-        public static IEnumerable<string> GetValueList(string token, string claimType)
+        /// <summary>
+        /// Obtém apenas 1 (um) valor correspondente a claimType solicitada.
+        /// Caso ocorra erro ao obter, retorna uma null.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="claimType"></param>
+        /// <returns></returns>
+        public static string GetValueToken(this string token, string claimType)
         {
-            List<string> listValues = new List<string>();
-
-            if (token.IsNullOrEmptyOrWhiteSpace() == false)
+            try
             {
-                JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
-                JwtSecurityToken jwtToken = handler.ReadJwtToken(token);
-                List<Claim> listClaim = jwtToken?.Claims.Where(x => x.Type == claimType || x.Type.Contains(claimType)).ToList();
+                string value = string.Empty;
 
-                if (listClaim != null)
+                if (token.IsNullOrEmptyOrWhiteSpace() == false)
                 {
-                    listClaim.ForEach(item => { listValues.Add(item.Value); });
-                }
-            }
+                    JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+                    JwtSecurityToken jwtToken = handler.ReadJwtToken(token);
 
-            return listValues;
+                    if (jwtToken != null)
+                    {
+                        Claim claim = jwtToken.Claims.Where(x => x.Type.ToUpper() == claimType.ToUpper() ||
+                                                                 x.Type.ToUpper().Contains(claimType.ToUpper())).FirstOrDefault();
+                        value = claim?.Value;
+                    }
+                }
+
+                return value;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Obtém uma lista de claims correspondente a claimType solicitada.
+        /// Caso ocorra erro ao obter, retorna uma null.
+        /// </summary>
+        /// <param name="token"></param>
+        /// <param name="claimType"></param>
+        /// <returns></returns>
+        public static IEnumerable<string> GetListValueToken(this string token, string claimType)
+        {
+            try
+            {
+                List<string> listValues = new List<string>();
+
+                if (token.IsNullOrEmptyOrWhiteSpace() == false)
+                {
+                    JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+                    JwtSecurityToken jwtToken = handler.ReadJwtToken(token);
+
+                    if (jwtToken != null)
+                    {
+                        List<Claim> listClaim = jwtToken.Claims.Where(x => x.Type.ToUpper() == claimType.ToUpper() ||
+                                                                           x.Type.ToUpper().Contains(claimType.ToUpper())).ToList();
+                        
+                        listClaim?.ForEach(item => { listValues.Add(item.Value); });
+                    }
+                }
+
+                return listValues;
+            }
+            catch
+            {
+                return null;
+            }
         }
 
     }
