6a2b0c81c6287ec2cc1702bee2be7082a974f71f
Rodrigo de Almeida Francisco <rodrigoalmeidafrancisco@gmail.com>
2023-06-01 00:21:40 -0300

    Criado mais métodos

--- Project/Developer.ExtensionCore/NewtonsoftJsonExtension.cs
+++ Project/Developer.ExtensionCore/NewtonsoftJsonExtension.cs
@@ -1,5 +1,4 @@
 ﻿using Newtonsoft.Json;
-using System;
 
 namespace Developer.ExtensionCore
 {
@@ -13,16 +12,30 @@ namespace Developer.ExtensionCore
         /// <returns></returns>
         public static string ToNewtonsoftSerializeJson(this object obj)
         {
+            string valueReturn = obj.ToNewtonsoftSerializeJson(new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+
+            return valueReturn;
+        }
+
+        /// <summary>
+        /// Serializa o Objeto em JSON usando a dll 'Newtonsoft.Json'.
+        /// Caso não consiga converter, retorna null.
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="settings"></param>
+        /// <returns></returns>
+        public static string ToNewtonsoftSerializeJson(this object obj, JsonSerializerSettings settings)
+        {
             string valueReturn = null;
 
             try
             {
                 if (obj != null)
                 {
-                    valueReturn = JsonConvert.SerializeObject(obj, new JsonSerializerSettings
-                    {
-                        ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-                    });
+                    valueReturn = JsonConvert.SerializeObject(obj, settings);
                 }
             }
             catch
@@ -42,16 +55,31 @@ namespace Developer.ExtensionCore
         /// <returns></returns>
         public static T ToNewtonsoftDeserializeJson<T>(this string json) where T : class
         {
+            T obj = json.ToNewtonsoftDeserializeJson<T>(new JsonSerializerSettings
+            {
+                ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+            });
+
+            return obj;
+        }
+
+        /// <summary>
+        /// Desserializa o JSON usando a dll 'Newtonsoft.Json'.
+        /// Caso não consiga converter, retorna null.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="json"></param>
+        /// <param name="settings"></param>
+        /// <returns></returns>
+        public static T ToNewtonsoftDeserializeJson<T>(this string json, JsonSerializerSettings settings) where T : class
+        {
             T obj = null;
 
             try
             {
                 if (json.IsNullOrEmptyOrWhiteSpace() == false)
                 {
-                    obj = JsonConvert.DeserializeObject<T>(json, new JsonSerializerSettings
-                    {
-                        ReferenceLoopHandling = ReferenceLoopHandling.Ignore
-                    });
+                    obj = JsonConvert.DeserializeObject<T>(json, settings);
                 }
             }
             catch
@@ -61,7 +89,6 @@ namespace Developer.ExtensionCore
 
             return obj;
         }
-
 
     }
 }
